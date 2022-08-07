0b74f01d37a77e6a0a962a83cf2ea4dee4a05ab3
12Gustavo21 <gustavo927318@gmail.com>
2022-08-07 15:07:09 -0300

    Flavo-project

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const money = document.querySelector(".money")
-let priceOne = 15
-let priceTwo = 30
+const Body = document.querySelector("body");
+const money = document.querySelector(".money");
+const quantity = document.querySelector(".quantity");
+let Shopping = document.querySelector(".Shopping");
+let BagInput = document.querySelector("#Bag");
+let JarInput = document.querySelector("#Jar");
+let priceOne = 15;
+let priceTwo = 30;
+let counter = 1;
 
 function firstPrice() {
-  money.innerText = ("$") + (priceOne * counter)
+  money.innerText = "$" + priceOne * counter;
 }
 
 function secondPrice() {
-  money.innerText = ("$") + (priceTwo * counter)
+  money.innerText = "$" + priceTwo * counter;
 }
 
 function subtraction1() {
-  money.innerText = ("$") + (counter * priceOne)
+  money.innerText = "$" + counter * priceOne;
 }
 
 function subtraction2() {
-  money.innerText = ("$") + (counter * priceTwo)
+  money.innerText = "$" + counter * priceTwo;
 }
-
-const quantity = document.querySelector(".quantity")
-let counter = 1
 
 function add() {
   if (counter < 20) {
     counter = counter + 1
     quantity.innerText = counter
-  firstPrice();
-  secondPrice();
+    if (BagInput.checked == true) {
+      firstPrice();
+    } else if (JarInput.checked == true) {
+      secondPrice();
+    }
   }
 }
 
@@ -34,21 +40,23 @@ function remove() {
   if (counter > 1) {
     counter = counter - 1
     quantity.innerText = counter
-    subtraction1();
-    subtraction2();
+    if (BagInput.checked == true) {
+      subtraction1();
+    } else if (JarInput.checked == true) {
+      subtraction2();
+    }
   }
 }
 
 function show() {
-  let window = document.querySelector(".Window")
-  let Bag = document.querySelector(".Bag")
-  Bag.style.background = '#ff2828';
-  Bag.style.borderRadius = '10px';
+  let window = document.querySelector(".Window");
+  Shopping.style.background = '#ff2828';
+  Shopping.style.borderRadius = '10px';
   window.style.display = 'block';
 }
 
 function Cart() {
-  let window = document.querySelector(".Window")
+  let window = document.querySelector(".Window");
   window.style.display = 'none';
 }
 
